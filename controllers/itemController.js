6e2d2557a29784e8be6e498d7d763ce308693089
dovimaj <dovimajowka@gmail.com>
2021-01-14 23:23:12 +0200

    item create function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -15,11 +15,34 @@ exports.index = function (req, res) {
       item_list: function (callback) {
         Item.find({}, "img_url name category").limit(10).exec(callback);
       },
+      category_list: function (callback) {
+        Category.find({}, "name").exec(callback);
+      },
     },
     function (err, results) {
       res.render("index", {
         title: "Home",
         error: err,
+        data: results,
+      });
+    }
+  );
+};
+
+exports.item_create_get = async function (req, res) {
+  // Get all authors and genres, which we can use for adding to our book.
+  async.parallel(
+    {
+      category_list: function (callback) {
+        Category.find({}, "name").exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      res.render("item_create", {
+        title: "Create Book",
         data: results,
       });
     }
