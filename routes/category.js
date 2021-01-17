ef12f5ce51e83ac5ae9c12921c114f988f51b0cf
dovimaj <dovimajowka@gmail.com>
2021-01-17 22:40:37 +0200

    UI changes

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
-
-// Require category controller modules.
 const category_controller = require("../controllers/categoryController");
 
+// /// Category ROUTES ///
+
+// // GET request for creating a category. NOTE This must come before route that displays category (uses id).
 router.get("/new", category_controller.create_category_get);
 
+// //POST request for creating category.
+router.post("/new", category_controller.category_create_post);
+
+// // GET request for one category.
 router.get("/:name", category_controller.get_category_items);
+
+// // GET request to delete category.
+// router.get("/:id/delete", category_controller.category_delete_get);
+
+// // POST request to delete category.
+// router.post("/:id/delete", category_controller.category_delete_post);
+
+// // GET request to update category.
+// router.get("/:id/update", category_controller.category_update_get);
+
+// // POST request to update category.
+// router.post("/:id/update", category_controller.category_update_post);
 
 module.exports = router;
