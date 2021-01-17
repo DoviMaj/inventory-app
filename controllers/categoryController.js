0675a3ae191fc44b2848f825d9db9ca9a3b4c4fc
dovimaj <dovimajowka@gmail.com>
2021-01-17 22:37:34 +0200

    new category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,6 +3,41 @@ const Item = require("../models/item");
 const { body, validationResult } = require("express-validator");
 const async = require("async");
 
+exports.get_category_items = async function (req, res, next) {
+  async.parallel(
+    {
+      item_list: function (callback) {
+        Item.find({}).exec(callback);
+      },
+      category_list: function (callback) {
+        Category.find({}).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results == null) {
+        // No results.
+        var err = new Error("Item not found");
+        err.status = 404;
+        return next(err);
+      }
+      const item_list = results.item_list.filter(
+        (item) => item.category.name === req.params.name
+      );
+      let data = results;
+      data.item_list = item_list;
+      console.log(results);
+      // Successful, so render.
+      res.render("index", {
+        title: req.params.name,
+        data,
+      });
+    }
+  );
+};
+
 exports.create_category_get = function (req, res, next) {
   async.parallel(
     {
@@ -28,36 +63,68 @@ exports.create_category_get = function (req, res, next) {
   );
 };
 
-exports.get_category_items = async function (req, res, next) {
-  async.parallel(
-    {
-      item_list: function (callback) {
-        Item.find({}).exec(callback);
+exports.category_create_post = [
+  // (req, res, next) => {
+  //   if (!(req.body.category instanceof Array)) {
+  //     if (typeof req.body.category === "undefined") req.body.category = [];
+  //     else req.body.category = new Array(req.body.category);
+  //   }
+  //   next();
+  // },
+
+  // Validate and sanitise fields.
+  body("category", "Title must not be empty.")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("description", "Description must not be empty.")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a category object with escaped and trimmed data.
+    const category = new Category({
+      name: req.body.category,
+      description: req.body.description,
+      _id: req.body.id,
+    });
+
+    // Get all authors and genres for form.
+    async.parallel(
+      {
+        category_list: function (callback) {
+          Category.find({}).exec(callback);
+        },
       },
-      category_list: function (callback) {
-        Category.find({}).exec(callback);
-      },
-    },
-    function (err, results) {
-      if (err) {
-        return next(err);
+      function (err, results) {
+        if (err) {
+          return next(err);
+        }
+        // There are errors. Render form again with sanitized values/error messages.
+        if (!errors.isEmpty()) {
+          res.render("category_create", {
+            data: results,
+            name: req.body.category,
+            description: req.body.description,
+            errors: errors.array(),
+          });
+        } else {
+          // Data from form is valid. Save .
+          category.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            //successful - redirect to new category.
+            res.redirect(category.url);
+          });
+        }
       }
-      if (results.item_list == null) {
-        // No results.
-        var err = new Error("Item not found");
-        err.status = 404;
-        return next(err);
-      }
-      const item_list = results.item_list.filter(
-        (item) => item.category.name === req.params.name
-      );
-      let data = results;
-      data.item_list = item_list;
-      // Successful, so render.
-      console.log(item_list, "im, cat");
-      res.render("index", {
-        data,
-      });
-    }
-  );
-};
+    );
+    return;
+  },
+];
